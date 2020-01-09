c60b05f9ac122fc1846ec830c6a81b7936feba17
Adam Lemelin <akamu84@gmail.com>
2020-01-08 21:12:29 -0600

    Updates

---
+++ precache-manifest.219b06f56412dc0fa93bf2f67638c992.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4a2a33bdbed6d46cd06c483e88b4e9",
+    "url": "/board-game-collection/index.html"
+  },
+  {
+    "revision": "ed224a21484d4ee71024",
+    "url": "/board-game-collection/static/js/2.ad6cda56.chunk.js"
+  },
+  {
+    "revision": "6323b4d03a5046f88ed7637a2c613fff",
+    "url": "/board-game-collection/static/js/2.ad6cda56.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a68385da88f1aa1021f",
+    "url": "/board-game-collection/static/js/main.baaad1cf.chunk.js"
+  },
+  {
+    "revision": "bf857d79658c6eb5aee7",
+    "url": "/board-game-collection/static/js/runtime-main.4ba203cd.js"
+  }
+]);
